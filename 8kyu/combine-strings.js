ab981ea83889b493a0124eea7b28ee74d628fae7
Alexandr Smirnov <bearmd93@gmail.com>
2024-02-14 17:30:30 +0200

    combine-strings

---
+++ 8kyu/combine-strings.js
@@ -0,0 +1,7 @@
+//Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+function combineNames(firstName, lastName) {
+  return firstName + ' ' + lastName;
+}
+
+console.log('James', 'Stevens');

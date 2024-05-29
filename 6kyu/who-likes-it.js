35d13ee710f54f301268670133b38ea4640e1779
Alexandr Smirnov <bearmd93@gmail.com>
2024-05-29 10:52:39 +0300

    Up !

--- 6kyu/who-likes-it.js
+++ 6kyu/who-likes-it.js
@@ -9,9 +9,33 @@
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
 
 function likes(names) {
-  // TODO
+  if (names.length == 0) return "no one likes this";
+  else if (names.length == 1) return `${names[0]} likes this`;
+  else if (names.length == 2) return `${names[0]} and ${names[1]} like this`;
+  else if (names.length == 3)
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  else
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
 }
 
-console.log([]);
-console.log(["Peter"]);
-console.log(["Jacob", "Alex"]);
+console.log(likes([]));
+console.log(likes(["Peter"]));
+console.log(likes(["Jacob", "Alex"]));
+console.log(likes(["Max", "John", "Mark"]));
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]));
+console.log(
+  likes([
+    "Alex",
+    "Jacob",
+    "Mark",
+    "Max",
+    "Alex",
+    "Jacob",
+    "Mark",
+    "Max",
+    "Alex",
+    "Jacob",
+    "Mark",
+    "Max",
+  ])
+);

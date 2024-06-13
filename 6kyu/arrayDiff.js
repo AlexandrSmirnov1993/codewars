82087c30f1034e2dfbad778ee38d01081a6c946e
Alexandr Smirnov <bearmd93@gmail.com>
2024-06-13 22:08:19 +0300

    in-process

--- 6kyu/arrayDiff.js
+++ 6kyu/arrayDiff.js
@@ -7,7 +7,9 @@
 
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
-function arrayDiff(a, b) {}
+function arrayDiff(a, b) {
+  return a.filter((el) => !b.includes(el));
+}
 
 console.log(arrayDiff(([1, 2], [1])));
 console.log(arrayDiff(([1, 2, 2, 2, 3], [2])));

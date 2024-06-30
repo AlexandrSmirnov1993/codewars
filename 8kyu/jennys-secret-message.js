d62ce8ffebd8b4149c76b4967936848a4edddd04
Alexandr Smirnov <bearmd93@gmail.com>
2024-06-30 23:17:23 +0300

    Up

--- 8kyu/jennys-secret-message.js
+++ 8kyu/jennys-secret-message.js
@@ -2,6 +2,12 @@
 
 // Can you help her?
 
+// function greet(name){
+//     return "Hello, " + name + "!";
+//     if(name === "Johnny")
+//       return "Hello, my love!";
+//   }
+
 function greet(name) {
   if (name === "Johnny") return "Hello, my love!";
   else return "Hello, " + name + "!";
